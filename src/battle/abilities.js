6051b0b682bfbca024a3a81abde2033bb8ff643f
umihyo24 <umihyo.corporation@gmail.com>
2026-05-07 16:37:35 +0900

    Delete src directory

--- src/battle/abilities.js
+++
@@ -1,7 +0,0 @@
-import { ABILITIES } from '../data/abilities.js';
-
-export function applyOnEnterAbility(unit) {
-  const ability = ABILITIES[unit.abilityId];
-  if (!ability || ability.triggerType !== 'onEnter') return;
-  unit.tempAtk += ability.atkBoost || 0;
-}
--- src/ui/dom.js
+++
@@ -1,8 +0,0 @@
-export function qs(sel, root = document) { return root.querySelector(sel); }
-export function el(tag, className = '', text = '') {
-  const node = document.createElement(tag);
-  if (className) node.className = className;
-  if (text) node.textContent = text;
-  return node;
-}
-export function clear(node) { node.replaceChildren(); }
