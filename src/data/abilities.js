4bf4eb9ce2064cbe418a098017110ca69d54ad0b
umihyo24 <umihyo.corporation@gmail.com>
2026-05-07 16:42:46 +0900

    Delete src directory

--- src/data/abilities.js
+++
@@ -1,5 +0,0 @@
-export const ABILITIES = {
-  iron_fist: { id: 'iron_fist', name: 'Iron Fist', triggerType: 'passive' },
-  battle_cry: { id: 'battle_cry', name: 'Battle Cry', triggerType: 'onEnter', atkBoost: 4 },
-  hardy: { id: 'hardy', name: 'Hardy', triggerType: 'passive' }
-};
